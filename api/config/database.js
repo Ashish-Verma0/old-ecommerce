496bcbeb7ef5e64a9f845d085a3e47f33482e45f
Ashish Verma <av84770@gmail.com>
2022-09-16 17:05:11 +0530

    small changes

--- api/config/database.js
+++ api/config/database.js
@@ -8,11 +8,11 @@ mongoose.connection.on('err', () => {
     console.log('err')
 })
 
-databaseUrl = "mongodb+srv://Ecommerce:wdc07HIzMoOUQtcn@cluster0.3hdmt.mongodb.net/Ecommerce?retryWrites=true&w=majority"
+// databaseUrl = "mongodb+srv://Ecommerce:wdc07HIzMoOUQtcn@cluster0.3hdmt.mongodb.net/Ecommerce?retryWrites=true&w=majority"
 
 const connectDatabase = () => {
 
-    mongoose.connect(databaseUrl, {
+    mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
